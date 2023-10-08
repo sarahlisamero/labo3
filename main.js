658ae5f4797e486ddf045301335cba3d1f4b47fd
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-10-08 18:50:24 +0200

    feat: show drink from new api related to the weather

--- main.js
+++ main.js
@@ -24,20 +24,67 @@ class App {
       let message = "";
         switch (true) {
           case currentTemperature < 10:
-            message = `${currentTemperature} It's cold`;
+            message = `Right now it is ${currentTemperature} degrees. That's cold brrr!`;
             break;
           case currentTemperature >= 10 && currentTemperature <= 20:
-            message = `${currentTemperature} It's chilly`;
+            message = `Right now it is ${currentTemperature} degrees. That's a bit chilly!`;
             break;
           case currentTemperature >= 20:
-            message = `${currentTemperature} It's warm`;
+            message = `Right now it is ${currentTemperature} degrees. That's warm, puff puff!`;
             break;
         }
       document.querySelector("#app").innerHTML = message;
+      this.recommendCocktail(currentTemperature);
     })
     .catch(err => {
       console.log(err);
     });
+  }
+  recommendCocktail(currentTemperature) {
+    let cocktailCategory;
+
+    switch (true) {
+      case currentTemperature < 10:
+        mcocktailCategory = "Cocoa";
+        break;
+      case currentTemperature >= 10 && currentTemperature <= 20:
+        cocktailCategory = "Ordinary_Drink";
+        break;
+      case currentTemperature >= 20:
+        cocktailCategory = "Cocktail";
+        break;
+    }
+    
+    this.fetchRandomCocktail(cocktailCategory);
+  }
+  fetchRandomCocktail(cocktailCategory) {
+    let cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cocktailCategory}`;
+
+    fetch(cocktailUrl)
+      .then(response =>{ 
+        return response.json()
+      })
+      .then(cocktailData => {
+        if (cocktailData.drinks && cocktailData.drinks.length > 0) {
+          const randomIndex = Math.floor(Math.random() * cocktailData.drinks.length);
+          const cocktail = cocktailData.drinks[randomIndex];
+          const cocktailName = cocktail.strDrink;
+
+          document.querySelector("#drink").innerHTML = `Recommended Cocktail for temperature: ${cocktailName}`;
+
+          const cocktailImage = cocktail.strDrinkThumb;
+          const image = new Image();
+          image.setAttribute("src", cocktailImage);
+          image.setAttribute("alt", cocktailName);
+          document.querySelector("#drinkImage").appendChild(image);
+
+        } else {
+          document.querySelector("#drink").innerHTML =`No cocktails found for this temperature.`;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   errorLocation(err){
     console.log(err);
